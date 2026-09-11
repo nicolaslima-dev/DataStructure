1572abec7369c7e225bf834c1573240103972f09
nicolaslima-dev <nicolassilvap1209@gmail.com>
2026-09-10 23:54:35 -0300

    add exemplos de pilha - Aula 01 e 02

---
+++ src/Pilha.java
@@ -0,0 +1,81 @@
+public class Pilha
+{
+	private int n;
+	private char vetor[];
+	private int topo;
+	
+	public Pilha()
+	{
+		n = 10;
+		vetor = new char[n];
+		topo = -1;
+	}
+	
+	public Pilha(int tamanho)
+	{
+		n = tamanho;
+		vetor = new char[tamanho];
+		topo = -1;
+	}
+	
+	public boolean vazia()
+	{
+		return topo == -1 ? true : false;
+	}
+	
+	public boolean cheia()
+	{
+		return topo == n - 1 ? true : false;
+	}
+	
+	public char pop()
+	{
+		char c = '\0';
+		
+		if (!this.vazia())
+		{
+			c = vetor[topo];
+			
+			//Decrementando o topo, o elemento  "removido"
+			topo--;
+		}
+		else
+		{
+			//Impress�o para fins didaticos
+			System.out.println("Pilha vazia: pop nao funcionou.");
+		}
+		
+		return c;
+	}
+	
+	public boolean push(char elemento)
+	{
+		if (!this.cheia())
+		{
+			vetor[++topo] = elemento;
+			return true;
+		}
+		else
+		{
+
+			System.out.println("Pilha cheia: push nao funcionou.\n");
+			return false;
+		}
+	}
+	
+	public char retornaTopo()
+	{
+		char elemento = '\0';
+
+		if(!this.vazia())
+		{
+			elemento = vetor[topo];         
+		}
+		else
+		{
+			System.out.println("Pilha vazia.");
+		}
+
+		return elemento;
+	}
+}
